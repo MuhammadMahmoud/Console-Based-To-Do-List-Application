55b4ae0860f4c63406afd783c04a038684779e4e
Muhammad Mahmoud <Muhammad.Abdo@cis.asu.edu.eg>
2024-05-26 22:57:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Console_Based_To_Do_List_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class ToDo
+    {
+        private string Name;
+        private string Description;
+        public ToDo() {
+            Name = "";
+        }
+
+        
+    }
+}
